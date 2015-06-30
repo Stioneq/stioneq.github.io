648ed8db41d857a02d462a32295df3a75bd52d9a
rlapin <stioneq@gmail.com>
2015-06-30 15:56:31 +0300

    Table created Added table filter support

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 $(document).ready(function() {
     var oldValue;
+
+    /**
+     * Sort table
+     * @param col - sorted column
+     * @param isAsc - if @true , asc sort , else descent
+     */
+    function sortTable(col, isAsc) {
+
+        var list = $("td:nth-child("+col+")").map(function(ev){
+            return $(this).html();
+        });
+        if(isAsc){
+            list.sort();
+        }else {
+            list.sort(function sort(a, b) {
+                if (a < b) {
+                    return 1;
+                } else if (a > b) {
+                    return -1;
+                }
+                return 0;
+
+            });
+        }
+
+        console.log(list.length)
+
+        console.log(list);
+
+    }
+
     $("th").click(function () {
+        var header = $(this);
+        $("th").not(this).removeClass("asort").removeClass("dsort");
+        if(header.hasClass("asort")){
+            header.removeClass("asort");
+
+            header.addClass("dsort");
+        }else{
+            header.removeClass("dsort");
+            header.addClass("asort");
+        }
+        sortTable(header.index() + 1,header.hasClass("asort"));
+
+
     });
     jQuery.expr[":"].Contains = jQuery.expr.createPseudo(function(arg) {
         return function( elem ) {
@@ -16,7 +60,7 @@ $(document).ready(function() {
         if(oldValue != "") {
             $("tr:Contains("+oldValue+")").show();
             $("tr:not(:Contains("+oldValue+"))").hide();
-            $("td:not(:Contains("+oldValue+"))").removeClass("filterValue")
+            $("td:not(:Contains("+oldValue+"))").removeClass("filterValue");
             $("td:Contains("+oldValue+")").addClass("filterValue");
         }else{
             $("td").removeClass("filterValue");
